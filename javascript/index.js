25791472e8381bc7e950906863d29e10167630bc
zuzhus <zuzana.barlogova@gmail.com>
2023-04-01 19:25:01 +0200

    added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ function updateTime() {
 
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
   losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
-    "h:mm:ss"
+    "h:mm:ss "
   )} <small>${losAngelesTime.format("A")}</small>`;
 
   //Paris
@@ -18,8 +18,29 @@ function updateTime() {
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
   parisTimeElement.innerHTML = `${parisTime.format(
-    "h:mm:ss"
+    "h:mm:ss "
   )} <small>${parisTime.format("A")}</small>`;
 }
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm ss "
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>
+      </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
